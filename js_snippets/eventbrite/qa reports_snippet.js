9aa652f60d9f663393859063bffb369468e95415
mistahchris <mistahcummings@gmail.com>
2017-01-24 18:14:33 -0600

    add qa versions of snippets

---
+++ js_snippets/eventbrite/qa reports_snippet.js
@@ -0,0 +1,25 @@
+var ENV = "evbqa";
+var PATTERN = /\b[0-9]{11}\b/g;
+var app = Application.currentApplication();
+app.includeStandardAdditions();
+var INPUT = app.theClipboard();
+var ID = INPUT.match(PATTERN);
+
+if (!ID) {
+	var message = "Please copy an event ID and try again.";
+
+	app.displayNotification(message, {
+		withTitle: "Reports Lookup",
+		subtitle: "Not an Event ID",
+		soundName: "text"
+	});
+} else {
+	ID = ID[0];
+	var URL = "https://" + ENV + ".com/myevent/" + ID + "/reports/";
+	var chrome = Application('Google Chrome');
+	var tab = chrome.Tab({url: URL});
+
+	chrome.windows[0].tabs.push(tab);
+	chrome.activate();
+}
+ignoreOutput;
