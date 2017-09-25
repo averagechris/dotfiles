999ffbea68cf57346cdb3a62bac5522c7d59d1bc
mistahchris <mistahcummings@gmail.com>
2017-09-25 10:22:30 -0500

    blah

--- test/triage_dashboard/mytest.js
+++ test/triage_dashboard/mytest.js
@@ -1,1 +1,25 @@
-alert('hahahahahahahaha');
+let my_url = '';
+let my_data = {};
+
+function shit(url, data) {
+    fetch(url, {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(data),
+    })
+        .then(response => {
+            let reader = response.body.getReader();
+            let decoder = new TextDecoder();
+
+            reader.read().then(d => {
+                if (d.done) {
+                    return;
+                }
+        console.log(decoder.decode(d.value, {stream: true})); // eslint-disable-line
+            });
+        })
+    .catch(response => console.log('there was an error:', response)); // eslint-disable-line
+}
+
+shit(my_url, my_data);
