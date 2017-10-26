69a78f16ce2bc7ad20e3b2c9db7827cca366a415
mistahchris <mistahcummings@gmail.com>
2017-10-26 14:12:32 -0500

    add python path variables to shell initilization, add them to path variable so that python tools in .pyenv*/bin work in neovim

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,7 @@
-set PATH $HOME/.cargo/bin $PATH
+set PYTHON_2_PATH $HOME/.pyenv/versions/misc2/bin
+set PYTHON_3_PATH $HOME/.pyenv/versions/misc3/bin
+set RUSTPATH $HOME/.cargo/bin
+set -x PATH $RUSTPATH $PYTHON_2_PATH $PYTHON_3_PATH $PATH
 set FZF_DEFAULT_COMMAND 'rg --files --hidden --follow'
 
 fish_vi_key_bindings
