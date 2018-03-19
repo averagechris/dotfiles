71f153de19e9af1c36f6180a89af44ab10f4bfb6
mistahchris <mistahcummings@gmail.com>
2018-03-19 17:27:12 -0500

    add quote for urls to simple_quote

--- fish/functions/simple_quote.fish
+++ fish/functions/simple_quote.fish
@@ -1,9 +1,21 @@
 function simple_quote
+  error_if_not_set URL_REGEX
+
   for arg in $argv
+    set should_quote false
+
     if echo $arg | contains_spaces
-      echo "'$arg'"
+      set should_quote true
+    end
+
+    if echo $arg | grep -Eq "$URL_REGEX"
+        set should_quote true
+    end
+
+    if eval $should_quote
+        echo "'$arg'"
     else
-      echo $arg
+        echo $arg
     end
   end
 end
