d055b128d9b98a84bf96ee6557f0f7244909765b
mistahchris <mistahcummings@gmail.com>
2018-04-04 13:43:38 -0500

    make nmshow +deps more robust to handle malformed json and email

--- fish/functions/nmshow.fish
+++ fish/functions/nmshow.fish
@@ -1,4 +1,4 @@
 function nmshow
     # tag:api_support and tag:api_answered and tag:cx_can_answer and from:support@eventbrite.com
-    notmuch-remote show --format=json $argv | parse_notmuch_email | parsed_email_jq
+    notmuch-remote show --format=json $argv | jq  . | parse_notmuch_email | parsed_email_jq
 end
