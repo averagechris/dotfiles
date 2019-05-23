095e8bfb7528f11afc3c3d7938562760ea94703a
mistahchris <chris@thesogu.com>
2019-05-23 01:05:10 -0500

    remove old te snippets

--- js_snippets/eventbrite/dev/github_snippet.js
+++
@@ -1,7 +0,0 @@
-var chrome = Application("Google Chrome"); chrome.includeStandardAdditions = true;
-var INPUT =  "\"".concat(chrome.theClipboard()).concat("\"");
-var URL = "https://github.com/eventbrite/core/search?utf8=✓&q=" + encodeURIComponent(INPUT);
-var chrome = Application("Google Chrome");var tab = chrome.Tab({url: URL});
-chrome.windows[0].tabs.push(tab);
-chrome.activate();
-ignoreOutput;
