a461420c946283758e80ebb536badf207a76dbb0
mistahchris <chris@thesogu.com>
2019-06-01 13:46:56 -0500

    add key binding for multiply-at-pt

--- spacemacs/layers/personal-layer/keybindings.el
+++ spacemacs/layers/personal-layer/keybindings.el
@@ -1,5 +1,6 @@
 (when (configuration-layer/package-usedp 'evil-numbers)
   (spacemacs/set-leader-keys
+    "o*" 'personal-layer/multiply-at-pt
     "op" 'personal-layer/to-private-event-id
     "oP" 'personal-layer/to-public-event-id
     "ou" 'personal-layer/to-private-user-id
