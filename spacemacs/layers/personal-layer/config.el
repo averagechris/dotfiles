690847f1e5545a3218fc322f01bf6b433fb97b3b
mistahchris <chris@thesogu.com>
2019-06-01 23:55:41 -0500

    add env vars for eb dirs

--- spacemacs/layers/personal-layer/config.el
+++ spacemacs/layers/personal-layer/config.el
@@ -3,9 +3,12 @@
 
 ;; default spacemacs overrides
 (setq-default dotspacemacs-themes '(spacemacs-light spacemacs-dark leuven))
-(setq dotspacemacs-helm-resize t)
-(setq dotspacemacs-maximized-at-startup t)
-(setq dotspacemacs-search-tools '("ag" "grep"))
-(setq dotspacemacs-whitespace-cleanup 'trailing)
+(setq
+ dotspacemacs-helm-resize t
+ dotspacemacs-maximized-at-startup t
+ dotspacemacs-search-tools '("ag" "grep")
+ dotspacemacs-whitespace-cleanup 'trailing
+ dotspacemacs-maximized-at-startup t
+ dotspacemacs-fullscreen-at-startup nil)
 
 (defvar personal-layer-use-black-formater nil "Toggle for running black on saving a buffer.")
