af8f96ef2b0e1e3d2629b8baa78b20c99d6157d1
ccummings-eb <ccummings@eventbrite.com>
2020-06-24 18:58:49 -0500

    fix something with the env variables for launchctl daemons

--- start_scripts/macos_launchctl_env_variables.sh
+++ start_scripts/macos_launchctl_env_variables.sh
@@ -1,6 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 launchctl setenv WORKON_HOME "$HOME/.local/share/virtualenvs/"
 launchctl setenv PATH "$PATH:/usr/local/bin:$HOME/dotfiles/bin:$HOME/.cargo/bin:$HOME/.pyenv/:$HOME/.pyenv/versions:$HOME/.pyenv/shims"
 launchctl setenv RUST_SRC_PATH "$HOME/.rustup/toolchains/stable-x86_64-apple-darwin/lib/rustlib/src/rust/src"
-
-# this is used in launchctl env variables start script
