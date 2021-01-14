251f13e9cefc45c53cca8cb2bc203fa270aa95b1
Chris Cummings <chris@thesogu.com>
2021-01-14 00:07:03 -0600

    remove unused configs

--- .profile
+++
@@ -1,6 +0,0 @@
-export PATH="/usr/local/bin/:$HOME/.cargo/bin:$PATH"
-export VISUAL="/usr/local/bin/emacsclient -c"
-
-export PATH="$HOME/.cargo/bin:$PATH"
-
-export PATH="$HOME/.poetry/bin:$PATH"
--- bash/functions/rm.bash
+++
@@ -1,8 +0,0 @@
-function rm() {
-    if echo "$@" | grep -qE '(^|\s+)(/|~)\*?\b'; then
-        echo "you do not want to delete / or ~ ... dumbass" > /dev/stderr
-        return 1
-    fi;
-
-    /bin/rm $argv
-}
