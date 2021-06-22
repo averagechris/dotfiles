6af605b0eb902ddba009e7c96d37613850ac1681
Chris Cummings <chris.cummings@sureapp.com>
2021-06-22 10:27:49 -0500

    Adds yabai keybinding refinements for macos

--- stowed/nixpkgs/.config/nixpkgs/python/default.nix
+++ stowed/nixpkgs/.config/nixpkgs/python/default.nix
@@ -1,19 +1,22 @@
 { pkgs, ... }:
 
-{
-  home.packages = with pkgs.python39Packages; [
-    black
-    # epc
-    # flake8
-    # importmagic
-    # isort
-    # pyflakes
-    # pytest
-    # python-language-server
-    ipdb
-    ipython
-    poetry
-    pre-commit
-    python
-  ];
+let isLinux = pkgs.stdenv.hostPlatform.isLinux;
+in {
+  if isLinux: then {
+    home.packages = with pkgs.python39Packages; [
+      black
+      epc
+      flake8
+      importmagic
+      isort
+      pyflakes
+      pytest
+      python-language-server
+      ipdb
+      ipython
+      poetry
+      pre-commit
+      python
+    ];
+  }
 }
