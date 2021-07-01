e0a783d18caa2492a15b06a8f4d7c27d7c41c5fa
Chris Cummings <chris@thesogu.com>
2021-06-30 21:06:41 -0500

    removes (format +onsave) from doom config
    doom format-all broke in more ways, this fixes it 😢

--- stowed/nixpkgs/.config/nixpkgs/doom/doom.d/init.el
+++ stowed/nixpkgs/.config/nixpkgs/doom/doom.d/init.el
@@ -47,7 +47,7 @@
        (evil +everywhere); come to the dark side, we have cookies
        file-templates    ; auto-snippets for empty files
        fold              ; (nigh) universal code folding
-       (format +onsave)  ; automated prettiness
+       ; (format +onsave)  ; automated prettiness
        multiple-cursors  ; editing in many places at once
        snippets          ; my elves. They type so I don't have to
        ;; word-wrap         ; soft wrapping with language-aware indent
