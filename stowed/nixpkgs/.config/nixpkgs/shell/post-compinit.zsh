acc538b36f4029db21482d07202b8e0db556a1cc
Chris Cummings <chris@thesogu.com>
2021-07-08 22:38:20 -0500

    add macos functions to fpath in shell

--- stowed/nixpkgs/.config/nixpkgs/shell/post-compinit.zsh
+++ stowed/nixpkgs/.config/nixpkgs/shell/post-compinit.zsh
@@ -5,10 +5,16 @@ function () {
     # lazy load functions defined in dotfiles/zsh/functions
     local fns=$HOME/dotfiles/zsh/functions
     local sure=$HOME/dotfiles/zsh/sure
+    local macosfns=$HOME/dotfiles/zsh/macos_functions
 
     fpath=($sure $fns $fpath)
     for fn in $(ls $fns); do autoload $fn; done;
     [[ -d $sure ]] && for fn in $(ls $sure); do autoload $fn; done;
+
+    if test $(uname) = "Darwin"; then
+        fpath=($fpath $macosfns)
+        [[ -d $macosfns ]] && for fn in $(ls $macosfns); do autoload $fn; done;
+    fi
 }
 
 eval "$(direnv hook zsh)"
