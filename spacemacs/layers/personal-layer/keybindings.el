821fefcab6f98e8ee11b6498ed3fec137c40f985
Chris Cummings <chris@thesogu.com>
2021-08-03 22:27:35 -0500

    remove spacemacs config

--- spacemacs/layers/personal-layer/keybindings.el
+++
@@ -1,9 +0,0 @@
-(spacemacs/declare-prefix "o" "personal-layer")
-
-(when (configuration-layer/package-usedp 'evil-numbers)
-  (spacemacs/set-leader-keys
-    "o*" 'personal-layer/multiply-at-pt
-    "op" 'personal-layer/to-private-event-id
-    "oP" 'personal-layer/to-public-event-id
-    "ou" 'personal-layer/to-private-user-id
-    "oU" 'personal-layer/to-public-user-id))
