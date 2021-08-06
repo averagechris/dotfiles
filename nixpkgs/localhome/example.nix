2785da4416cb8394db30b405a6bc62036180c512
Chris Cummings <chris@thesogu.com>
2021-08-05 23:53:13 -0500

    add context to localhome/example.nix

--- nixpkgs/localhome/example.nix
+++ nixpkgs/localhome/example.nix
@@ -14,10 +14,16 @@
 { config, pgks, ... }:
 
 {
-  config.home.username = "your_user_name";
+  ###
+  ### Tell home-manager the basicss
+  ###
+  config.home.username = "<username>";
+  # on macos this is /Users/<username>
   config.home.homeDirectory = "/home/chris";
 
-  # import whatever modules you want on this system
+  ###
+  ### Choose which modules to install on this system
+  ###
   imports = [
     ../emacs
     ../email
@@ -36,19 +42,37 @@
     ../yabai
   ];
 
+  ###
+  ### Setup the git personal info relevant for this system
+  ###
   config.programs.git = {
     userName = ":)";
     userEmail = "email@email.com";
   };
 
+  ###
+  ### Enable specific email accounts
+  ###
   config.accounts.email.accounts = {
     # add a new email account by adding a nix module, then adding it to the .gitignore
     # or ignore privately
     # see example-email.nix for an example
     # e.g.:
     # example = import ../email/example-email.nix;
+    # personal = import ../email/personal.nix
   };
 
+  ####
+  #### extra options to consider
+  ####
+
+  # if on a machine using aws cli you probably want to set this
+  # to the profile you use most often
+  programs.zsh.sessionVariables = {
+    AWS_PROFILE="????";
+  };
+
+  # if on pop-os you probably want to add these
   # make sure all of the distro's default XDG_DATA_DIRS values are in here
   # most of these should be set in linux_desktop/default.nix
   config.xdg.systemDirs.data = [
