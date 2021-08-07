78e2430edabaa0e9640d592cafef8b3b3f00f0c7
Chris Cummings <chris@thelio.lan>
2021-08-07 01:03:23 -0500

    add home-manager import to darwin config

--- nixpkgs/darwin/configuration.nix
+++ nixpkgs/darwin/configuration.nix
@@ -1,6 +1,8 @@
 { config, pkgs, ... }:
 
 {
+  imports = [ <home-manager/nix-darwin> ];
+
   # Use a custom configuration.nix location.
   # $ darwin-rebuild switch -I darwin-config=$HOME/.config/nixpkgs/darwin/configuration.nix
   environment.darwinConfig = "$HOME/.config/nixpkgs/darwin/configuration.nix";
