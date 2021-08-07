bb301790b0f65ce459cb3a1593d55de0eaad7afc
Chris Cummings <chris@thelio.lan>
2021-08-07 14:52:00 -0500

    add comment

--- nixpkgs/darwin/configuration.nix
+++ nixpkgs/darwin/configuration.nix
@@ -1,6 +1,6 @@
 { config, pkgs, ... }:
 {
-  imports = [ <home-manager/nix-darwin> ./local_config.nix];
+  imports = [ <home-manager/nix-darwin> ./local_config.nix ];
 
   # Use a custom configuration.nix location.
   # $ darwin-rebuild switch -I darwin-config=$HOME/.config/nixpkgs/darwin/configuration.nix
@@ -21,11 +21,15 @@
   # services.emacs.enable = true;
   # services.emacs.package = config.home-manager.users.christophercummings.programs.emacs.package;
 
+  # makes nix-darwin put handling in /etc/static/zsh* files so that NIX_PATH correctly
+  # references all of the nix-darwin stuff
+  # does not conflict with home-manager.programs.zsh
   programs.zsh.enable = true;
 
   # enable launchd daemon for mbsync to sync and index emails if emails are configured in home-manager config
-  launchd.user.agents.mbsync = if config.home-manager.users.christophercummings.accounts.email.accounts != {} then {
-    command = "${pkgs.isync}/bin/mbsync -a && mu ${pkgs.mu}/bin/mu index";
-    serviceConfig.StartInterval = 60 * 5;
-  } else {};
+  launchd.user.agents.mbsync =
+    if config.home-manager.users.christophercummings.accounts.email.accounts != { } then {
+      command = "${pkgs.isync}/bin/mbsync -a && mu ${pkgs.mu}/bin/mu index";
+      serviceConfig.StartInterval = 60 * 5;
+    } else { };
 }
