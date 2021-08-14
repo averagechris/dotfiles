b5dec46922c5ea1089c01de45e8091ce95c988fd
Chris Cummings <chris@thesogu.com>
2021-08-14 02:40:26 -0500

    enable firefox.GnomeExtentions in non macos

--- nixpkgs/firefox/default.nix
+++ nixpkgs/firefox/default.nix
@@ -1,23 +1,32 @@
 { pkgs, ... }:
-
-{
+let
+  isLinux = pkgs.stdenv.hostPlatform.isLinux;
+in {
   programs.firefox = {
     enable = true;
 
+    package = pkgs.firefox.override {
+      cfg = {
+        enableGnomeExtensions = isLinux;
+      };
+    };
+
     profiles.me = {
       name = "me";
-      settings = {
-        "browser.startup.homepage" = "https://duckduckgo.com";
-        "browser.search.region" = "US";
-        "browser.search.isUS" = true;
-        "browser.bookmarks.showMobileBookmarks" = true;
+      settings = if isLinux then {
+        # https://wiki.archlinux.org/title/HiDPI#Firefox
+        "layout.css.devPixelsPerPx" = "2.0";
+      } else {} // {
 
         # allows firefox to see userChrome.css etc
         "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
 
-        # https://wiki.archlinux.org/title/HiDPI#Firefox
-        "layout.css.devPixelsPerPx" = "2.0";
+        "browser.startup.homepage" = "https://duckduckgo.com";
+        "browser.search.region" = "US";
+        "browser.search.isUS" = true;
+        "browser.bookmarks.showMobileBookmarks" = true;
       };
+
       userChrome = builtins.readFile ./userChrome.css;
       userContent = ''
         /* Hide scrollbar */
