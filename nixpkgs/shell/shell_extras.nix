9b796b3f04020968b5aafa2f070bf1b02b2c9c6e
Chris Cummings <chris@thesogu.com>
2022-06-05 01:30:37 -0500

    ++ compose keybinds & systemctl service restarter

--- nixpkgs/shell/shell_extras.nix
+++ nixpkgs/shell/shell_extras.nix
@@ -1,4 +1,4 @@
-{ pkgs }: with pkgs; {
+{ pkgs }: with pkgs; rec {
   video_compress = writeShellApplication {
     name = "video_compress";
     runtimeInputs = [ handbrake ];
@@ -72,6 +72,79 @@
         | sed -e 's/^ *//' \
         | sed -e 's/ *$//'
     '';
+  };
+
+  list_systemd_services = writeShellApplication rec {
+    name = "list_systemd_services";
+    runtimeInputs = [ bemenu fzf gawk ];
+    text = ''
+      USER_OR_SYSTEM="user"
+      FUZZY_FINDER="fzf --multi --exact --reverse --tiebreak=index"
+
+      function echo_help () {
+        echo "Usage: $0 [options]"
+        echo Options:
+        echo "  -h, --help      show this message"
+        echo "  --user          passed to systemctl e.g. systemctl --user"
+        echo "  --system        passed to systemctl e.g. systemctl --system"
+        echo "  --fuzzy-finder  give an alternative fuzzy finder command"
+        echo "                  defaults to: $FUZZY_FINDER"
+        echo "                  bemenu is also supported. e.g."
+        echo "                    ${name} --fuzzy-finder bemenu --ignorecase --center --margin 10 --list 10"
+      }
+
+      while [ $# -gt 0 ] && [ -n "$1" ]; do
+        case "$1" in
+          --help|-h)
+            echo_help
+            ;;
+          --user|--system)
+            USER_OR_SYSTEM="$1"
+            ;;
+          --fuzzy-finder)
+            shift
+            FUZZY_FINDER="$*"
+            break
+            ;;
+          *)
+            echo_help
+            ;;
+        esac
+
+      if [ $# -gt 0 ]; then
+         shift
+      fi
+
+      done
+
+      systemctl "--$USER_OR_SYSTEM" list-unit-files --type=service --no-legend \
+        | eval -- "$FUZZY_FINDER" \
+        | awk '{print $1}'
+
+    '';
+  };
+
+  bemenu_list_systemd_services = writeShellApplication {
+    name = "bemenu_list_systemd_services";
+    runtimeInputs = [ list_systemd_services ];
+    text = "list_systemd_services --fuzzy-finder bemenu --ignorecase --center --margin 10 --list 10";
+  };
+
+  try_restart_systemd_user_services = writeShellApplication {
+    name = "try_restart_systemd_user_services";
+    runtimeInputs = [ list_systemd_services ];
+    text = ''
+      CHOICES="$(list_systemd_services "$@")"
+      if [ -n "$CHOICES" ]; then
+        systemctl --user try-restart "$CHOICES"
+      fi
+    '';
+  };
+
+  bemenu_try_restart_systemd_user_services = writeShellApplication {
+    name = "bemenu_try_restart_systemd_user_services";
+    runtimeInputs = [ try_restart_systemd_user_services ];
+    text = "try_restart_systemd_user_services --fuzzy-finder bemenu --ignorecase --center --margin 10 --list 10";
   };
 
 }
--- nixpkgs/sway/keybindings.nix
+++ nixpkgs/sway/keybindings.nix
@@ -12,41 +12,85 @@ let
       sed -e 's,dmenu -i,${pkgs.bemenu}/bin/bemenu -i -b -f -l 5,g' -i ./src/Main.hh
     '';
   });
+  leaveModeKeys = {
+    "Insert" = "mode default";
+    "Escape" = "mode default";
+    "Return" = "mode default";
+  };
+  setVolume = "exec ${pactl} set-sink-volume $(${pactl} list short sinks | grep RUNNING | cut -f 1)";
 
 in
 {
   config.wayland.windowManager.sway.config.modes = {
-    compose = {
-      "t" = ''exec swaymsg [app_id="scratch_terminal"] scratchpad show; mode default;'';
-      "f" = "fullscreen toggle; mode default;";
-      "Insert" = "mode default";
-      "Escape" = "mode default";
-      "Return" = "mode default";
+    disabled_mode = {
+      # a mode for ignoring all keybindings until the compose mode keys are
+      # repeated.
+      "Shift+space" = "mode default";
     };
-    resize = {
-      "h" = "resize shrink width 15 px";
-      "j" = "resize grow height 15 px";
-      "k" = "resize shrink height 15 px";
-      "l" = "resize grow width 15 px";
-      "Escape" = "mode default";
-      "Return" = "mode default";
-    };
+    compose_mode = {
+      # a mode for entering other modes, or inserting commands based on
+      # sequential key presses
+      # e.g. Shift+space -> k -> s  == bemenu_try_restart_systemd_user_services
+      f = "fullscreen toggle; mode default;";
+      k = "mode kill_mode";
+      r = "mode resize_mode";
+      t = ''exec swaymsg [app_id="scratch_terminal"] scratchpad show; mode default;'';
+      v = "mode volume_mode";
+      "Shift+space" = "mode disabled_mode";
+    } // leaveModeKeys;
+    kill_mode = {
+      q = "exec swaymsg exit";
+      r = "exec systemctl reboot -i";
+      s = "exec bemenu_try_restart_systemd_user_services; mode default;";
+      w = "kill; mode default;";
+    } // leaveModeKeys;
+    resize_mode = {
+      "${cfg.up}" = "resize shrink width 15 px";
+      "${cfg.down}" = "resize grow height 15 px";
+      "${cfg.left}" = "resize shrink height 15 px";
+      "${cfg.right}" = "resize grow width 15 px";
+      "Shift+${cfg.up}" = "resize shrink width 45 px";
+      "Shift+${cfg.down}" = "resize grow height 45 px";
+      "Shift+${cfg.left}" = "resize shrink height 45 px";
+      "Shift+${cfg.right}" = "resize grow width 45 px";
+    } // leaveModeKeys;
+    volume_mode = {
+      "${cfg.up}" = "${setVolume} +1%";
+      "Shift+${cfg.up}" = "${setVolume} +10%";
+      "${cfg.down}" = "${setVolume} -1%";
+      "Shift+${cfg.down}" = "${setVolume} -10%";
+    } // leaveModeKeys;
+    workspace_mode = {
+      "0" = "workspace 0";
+      "1" = "workspace 1";
+      "2" = "workspace 2";
+      "3" = "workspace 3";
+      "4" = "workspace 4";
+      "5" = "workspace 5";
+      "6" = "workspace 6";
+      "7" = "workspace 7";
+      "8" = "workspace 8";
+      "9" = "workspace 9";
+      "${cfg.right}" = "workspace next";
+      "${cfg.left}" = "workspace prev";
+    } // leaveModeKeys;
   };
+
   config.wayland.windowManager.sway.config.keybindings = {
     "${modkey}+t" = "exec ${cfg.terminal}";
     "${modkey}+q" = "kill";
     "${modkey}+space" = ''exec ${j4-dmenu-desktop}/bin/j4-dmenu-desktop'';
     "${modkey}+Shift+q" = "exec ${nwg-bar}/bin/nwg-bar";
 
-    "XF86LaunchB" = "exec ${nwg-drawer}/bin/nwg-drawer";
-    "XF86AudioPlay" = "exec ${playerctl} play-pause";
-    "XF86AudioNext" = "exec ${playerctl} next";
-    "XF86AudioPrev" = "exec ${playerctl} previous";
-    "XF86AudioLowerVolume" = "exec ${pactl} set-sink-volume $(${pactl} list short sinks | grep RUNNING | cut -f 1) -5%";
-    "XF86AudioRaiseVolume" = "exec ${pactl} set-sink-volume $(${pactl} list short sinks | grep RUNNING | cut -f 1) +5%";
-    "XF86AudioMute" = "exec ${pactl} set-sink-volume $(${pactl} list short sinks | grep RUNNING | cut -f 1) 0%";
-    "XF86MonBrightnessDown" = "exec ${brightnessctl} set 5%-";
-    "XF86MonBrightnessUp" = "exec ${brightnessctl} set +5%";
+    XF86LaunchB = "exec ${nwg-drawer}/bin/nwg-drawer";
+    XF86AudioPlay = "exec ${playerctl} play-pause";
+    XF86AudioNext = "exec ${playerctl} next";
+    XF86AudioPrev = "exec ${playerctl} previous";
+    XF86AudioLowerVolume = "${setVolume} -5%";
+    XF86AudioRaiseVolume = "${setVolume} +5%";
+    XF86AudioMute = "${setVolume} 0%";
+    XF86MonBrightnessDown = "exec ${brightnessctl} set 5%-";
+    XF86MonBrightnessUp = "exec ${brightnessctl} set +5%";
 
     # take a screenshot
     "${modkey}+ctrl+5" = ''exec grim -g "$(slurp)" - | swappy -f -'';
@@ -79,10 +123,10 @@ in
     "${modkey}+7" = "workspace number 7";
     "${modkey}+8" = "workspace number 8";
     "${modkey}+9" = "workspace number 9";
-    "${modkey}+ctrl+l" = "workspace next";
-    "${modkey}+ctrl+h" = "workspace prev";
-    "${modkey}+ctrl+j" = "workspace back_and_forth";
-    "${modkey}+ctrl+k" = "workspace back_and_forth";
+    "${modkey}+ctrl+${cfg.right}" = "workspace next";
+    "${modkey}+ctrl+${cfg.left}" = "workspace prev";
+    "${modkey}+ctrl+${cfg.down}" = "workspace back_and_forth";
+    "${modkey}+ctrl+${cfg.up}" = "workspace back_and_forth";
 
     "${modkey}+Shift+1" = "move container to workspace number 1";
     "${modkey}+Shift+2" = "move container to workspace number 2";
@@ -99,8 +143,7 @@ in
     # NOTE: to remove a window from scratch pad toggle floating
     # https://i3wm.org/docs/userguide.html#_scratchpad
 
-    "${modkey}+r" = "mode resize";
-    "Insert" = "mode compose";
+    "Shift+space" = "mode compose";
   };
 
   config.home.packages = [
