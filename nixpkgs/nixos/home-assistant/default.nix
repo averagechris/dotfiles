8b980e80cc11347e74593a4ab50b683e0cc5b8a7
Chris Cummings <chris@thesogu.com>
2023-02-18 00:31:55 -0600

    add basic initial home-assistant module

---
+++ nixpkgs/nixos/home-assistant/default.nix
@@ -0,0 +1,48 @@
+{pkgs, ...}: {
+  services.home-assistant = let
+    package =
+      (pkgs.home-assistant.override {
+        extraPackages = pythonPackages:
+          with pythonPackages; [
+            aiounifi # ubiquity router
+            aiowebostv # lg webOS tv
+            psycopg2 # postgres
+            pyatv # apple tv
+            pyicloud # apple icloud
+            python-miio # roborock
+            securetar # backup
+          ];
+      })
+      .overrideAttrs (oldAttrs: {
+        doInstallCheck = false;
+      });
+  in {
+    inherit package;
+    enable = true;
+    extraComponents = [
+      "met"
+      "radio_browser"
+      "roborock"
+    ];
+    config = {
+      # Includes dependencies for a basic setup
+      # https://www.home-assistant.io/integrations/default_config/
+      default_config = {};
+      recorder.db_url = "postgresql://@/hass";
+    };
+  };
+
+  services.postgresql = {
+    enable = true;
+    package = pkgs.postgresql_14;
+    ensureDatabases = ["hass"];
+    ensureUsers = [
+      {
+        name = "hass";
+        ensurePermissions = {
+          "DATABASE hass" = "ALL PRIVILEGES";
+        };
+      }
+    ];
+  };
+}
