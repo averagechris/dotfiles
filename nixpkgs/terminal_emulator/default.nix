7dc292a58f554ba477f5ef3cdf8498a8206fa0f6
Chris Cummings <chris@thesogu.com>
2023-05-30 02:05:59 -0500

    add theme to alacritty

--- nixpkgs/terminal_emulator/default.nix
+++ nixpkgs/terminal_emulator/default.nix
@@ -1,8 +1,4 @@
-{
-  pkgs,
-  config,
-  ...
-}: {
+{config, ...}: {
   config.programs.alacritty = {
     enable = true;
     settings = {
@@ -11,7 +7,13 @@
       # font.italic.family = "SourceCodePro";
       # font.bold_italic.family = "SourceCodePro";
       font.size = 16;
+      import = [
+        # TODO this should map from any theme file in ./themes/
+        "${config.xdg.configHome}/alacritty/themes/rose-pine.yml"
+      ];
     };
   };
   config.wayland.windowManager.sway.config.terminal = "${config.programs.alacritty.package}/bin/alacritty";
+  # https://github.com/rose-pine/alacritty/raw/main/dist/rose-pine-moon.yml
+  config.xdg.configFile."alacritty/themes/rose-pine.yml".source = ./themes/rose-pine.yml;
 }
--- nixpkgs/zellij/default.nix
+++ nixpkgs/zellij/default.nix
@@ -1,9 +1,13 @@
 {config, ...}: {
   programs.zellij.enable = true;
+  programs.gitui.enable = true;
+
   xdg.configFile."zellij/config.kdl".text =
     ''
       layout_dir "${config.xdg.configHome}/zellij/layouts"
       theme_dir "${config.xdg.configHome}/zellij/themes"
     ''
     + builtins.readFile ./config.kdl;
+
+  xdg.configFile."zellij/themes/rose-pine.kdl".source = ./themes/rose-pine.kdl;
 }
