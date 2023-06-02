8fd2a48d4ea80f513aca8d57eea9e0524c2340d1
Chris Cummings <chris@thesogu.com>
2023-06-02 16:18:18 -0500

    zellij: fix tabrename, add basic layout

--- nixpkgs/zellij/default.nix
+++ nixpkgs/zellij/default.nix
@@ -1,13 +1,25 @@
-{config, ...}: {
-  programs.zellij.enable = true;
-  programs.gitui.enable = true;
+{
+  config,
+  lib,
+  ...
+}: let
+  mkLayoutFile = file_name: {
+    xdg.configFile."zellij/layouts/${file_name}".source = "layouts/${file_name}";
+  };
+  merge = lib.foldl (a: b: a // b) {};
+  layouts = with builtins; map mkLayoutFile (attrNames (readDir ./layouts));
+in
+  merge layouts
+  // {
+    programs.zellij.enable = true;
+    programs.gitui.enable = true;
 
-  xdg.configFile."zellij/config.kdl".text =
-    ''
-      layout_dir "${config.xdg.configHome}/zellij/layouts"
-      theme_dir "${config.xdg.configHome}/zellij/themes"
-    ''
-    + builtins.readFile ./config.kdl;
+    xdg.configFile."zellij/config.kdl".text =
+      ''
+        layout_dir "${config.xdg.configHome}/zellij/layouts"
+        theme_dir "${config.xdg.configHome}/zellij/themes"
+      ''
+      + builtins.readFile ./config.kdl;
 
-  xdg.configFile."zellij/themes/rose-pine.kdl".source = ./themes/rose-pine.kdl;
-}
+    xdg.configFile."zellij/themes/rose-pine.kdl".source = ./themes/rose-pine.kdl;
+  }
