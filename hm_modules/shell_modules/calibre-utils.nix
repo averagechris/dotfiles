47e85fe7ae7fec7409b9334daa199d33525c88c5
Chris Cummings <chris@thesogu.com>
2025-01-11 22:13:46 -0600

    add backup feat to calibre-utils

--- hm_modules/shell_modules/calibre-utils.nix
+++ hm_modules/shell_modules/calibre-utils.nix
@@ -7,19 +7,60 @@
   cfg = config.dotfiles.shell.calibre-utils;
   calibre-utils = pkgs.writers.writePython3Bin "calibre-utils" {libraries = with pkgs.python3Packages; [tabulate typer rich];} ''
     import contextlib
+    import json
+    import os
     import re
     import statistics
+    import shutil
     import subprocess
+    import tempfile
+    from dataclasses import dataclass
+    from datetime import datetime
     from pathlib import Path
     from typing import Optional
     import typer
-    from rich.prompt import Confirm
+    from rich.prompt import Confirm, Prompt
     from rich import print
     from tabulate import tabulate
+
+
+    @dataclass
+    class Track:
+        num: int
+        start: int
+        end: int
+        title: str
+        bitrate: int
+        timebase: str = "1/1000"
+
+        def as_txt(self) -> str:
+            return (
+                f"[CHAPTER]\nTIMEBASE={self.timebase}\n"
+                f"START={self.start}\nEND={self.end}\n"
+                f"title={self.title}\n"
+                "\n"
+            )
+
+        def as_row(self) -> tuple[int, str, str]:
+            return (self.num, self.title, f"{int(self.bitrate / 1000)}k")  # noqa: E501
+
+        def __iadd__(self, other: "Track") -> None:
+            self.title = (
+                self.title
+                if len(self.title) < len(other.title)
+                else other.title
+            )
+            self.start = min((self.start, other.start))
+            self.end = max((self.end, other.end))
+            return None
+
+
     cli = typer.Typer()
     audio = typer.Typer(name="calibre-utils")
     cli.add_typer(audio, name="audio", help="Audio book related commands")
-    def get_duration_bitrate(filename: Path) -> Optional[tuple[int, int]]:  # noqa: E302,E501
+
+
+    def get_duration_bitrate(filename: Path) -> Optional[tuple[int, int]]:  # noqa: E501
         cmd: list[str] = [
             '${pkgs.ffmpeg}/bin/ffprobe',  # noqa: E501
             '-v', 'error',
@@ -47,7 +88,9 @@
         except Exception:
             print(f"[red]Unexpected output from ffprobe:[/red]\n{result.stdout}\n")
             return None
-    @audio.command()  # noqa: E302
+
+
+    @audio.command()
     def concat_tracks(
         name: str = typer.Argument(
             "notgiven",
@@ -74,58 +117,73 @@
         if not files:
             print(f"[yellow]No {pattern} files found in {directory}[/yellow]")
             raise typer.Exit(1)
-        metadata: list[str] = [";FFMETADATA1"]
-        chapter_titles: list[str] = []
+        tracks: dict[int, Track] = {}
         timestamp: int = 0
-        bitrates: list[int] = []
-        expected_file_sort_prefix = re.compile(r"^\d+ - ")
+        title_pattern = re.compile(r"^(?P<track_num>\d+) - (?P<title>.*)")
         for file in files:
+            title_match = re.match(title_pattern, file.stem)
+            if not title_match:
+                print("[red]Unable to extract track num and chapter title from file name[/red]")  # noqa: E501
+                print(f"use this title pattern: {title_pattern}")
+                raise typer.Exit(1)
             duration_bitrate = get_duration_bitrate(file)
             if duration_bitrate is None:
                 return None
             duration, bitrate = duration_bitrate
-            bitrates.append(bitrate)
-            chapter_title = re.sub(expected_file_sort_prefix, "", file.stem)
-            chapter: list[str] = [
-                "[CHAPTER]",
-                "TIMEBASE=1/1000",
-                f"START={timestamp}",
-                f"END={timestamp + duration}",
-                f"title={chapter_title}",
-                ""
-            ]
-            metadata.extend(chapter)
-            chapter_titles.append(chapter_title)
+            chapter_title = title_match["title"]
+            track_num = int(title_match["track_num"])
+            track = Track(
+                 num=track_num,
+                 start=timestamp,
+                 end=timestamp + duration,
+                 title=chapter_title,
+                 bitrate=bitrate,
+            )
+            if existing_track := tracks.get(track_num):
+                existing_track += track
+            else:
+                tracks[track_num] = track
             timestamp += duration
-        print(f"[green]✓[/green] Metadata collected from {len(files)} tracks.")
-        print(tabulate((
-                (i, title, bitrate)
-                for i, (title, bitrate)
-                in enumerate(zip(chapter_titles, bitrates))
-            ),
-            headers=["#", "Title", "Bitrate"],
-            tablefmt="simple")
+        print(f"[green]✓[/green] Metadata from {len(files)} files collecting into {len(tracks)} chapters/tracks.")  # noqa: E501
+
+        for i, track in enumerate(sorted(tracks.values(), key=lambda t: t.num)):
+            track.num = i + 1
+
+        print(
+            tabulate(
+                (track.as_row() for track in sorted(tracks.values(), key=lambda t: t.num)),  # noqa: E501
+                headers=["#", "Title", "Bitrate"],
+                tablefmt="simple",
+            )
         )
-        mean_bitrate = int(statistics.mean(bitrates) / 1000)
+        chapters_txt = directory / "chapters.txt"
+        try:
+            chapters_txt.write_text("")  # clear in case of prev run
+            with chapters_txt.open(mode="a") as fp:
+                fp.write(";FFMETADATA1\n")
+                for track_num in sorted(tracks):
+                    track = tracks[track_num]
+                    fp.write(track.as_txt())
+        except Exception as e:
+            print(f"[red]Error writing metadata file: {e}[/red]")
+            raise typer.Exit(3)
+        mbitrate = int(statistics.median(t.bitrate for t in tracks.values()) / 1000)  # noqa: E501
         # find the closest target bit rate from the mean bitrate
-        target_bitrate = min([24, 48, 64, 128, 256, 320], key=lambda n: abs(n-mean_bitrate))  # noqa: E501
-        print(f"[green]✓[/green] Target bitrate: {target_bitrate}kbps")
+        target_bitrate = min([24, 48, 64, 128, 256, 320], key=lambda n: abs(n-mbitrate))  # noqa: E501
+        print(f"[green]✓[/green] Target bitrate): {target_bitrate}kbps")
         if not Confirm.ask("[yellow]?[/yellow] Track list and bitrate look good?"):
             print("The files must be sorted in lexographical sort order. prepend the order.")  # noqa: E501
             print("e.g. [cyan]001 - Prologue.mp3[/cyan]")
             print("e.g. [cyan]002 - Chapter 1.mp3[/cyan]")
             print("e.g. [cyan]003 - Chapter 2.mp3[/cyan]")
+            print("e.g. [cyan]004 - Chapter 3 pt1.mp3[/cyan]")
+            print("e.g. [cyan]004 - Chapter 3 pt2.mp3[/cyan]")
             raise typer.Exit(2)
-        output_file = directory / "chapters.txt"
-        try:
-            output_file.write_text('\n'.join(metadata))
-        except Exception as e:
-            print(f"[red]Error writing metadata file: {e}[/red]")
-            raise typer.Exit(3)
         input_files = directory / "input_files.txt"
         input_files.write_text("\n".join(f"file '{f.as_posix()}'" for f in files))
         output_file_name = name if name != "notgiven" else directory.absolute().stem  # noqa: E501
-        print(f"[green]✓[/green] Merging files to create audio book [cyan]{output_file_name}[/cyan] @ {target_bitrate}kbps...")  # noqa: E501
+        output_file_name = Prompt.ask("Name of the book:", default=output_file_name)  # noqa: E501
+        print(f"[green]✓[/green] Merging files to create audio book [cyan]{output_file_name}.m4b[/cyan] @ {target_bitrate}kbps...")  # noqa: E501
         ffmpg_cmd = [
             "${pkgs.ffmpeg}/bin/ffmpeg",  # noqa: E501
             "-f",
@@ -135,7 +193,7 @@
             "-i",
             "input_files.txt",
             "-i",
-            f"{output_file}",
+            f"{chapters_txt}",
             "-map",
             "0:a",
             "-map_metadata",
@@ -170,7 +228,90 @@
                 input_files.unlink()
             with contextlib.suppress(Exception):
                 (Path(".") / "chapters.txt").unlink()
-    if __name__ == "__main__":  # noqa: E305
+
+
+    def _get_default_calibre_library_path() -> Path:
+        cfg_dir = Path(os.environ.get("XDG_CONFIG_HOME", "~/.config")).absolute()  # noqa: E501
+        if (cfg_file := cfg_dir / "calibre/global.py.json") and cfg_file.exists():
+            calibre_config = json.loads(cfg_file.read_text())
+            _lib_path = calibre_config.get("library_path")
+            if (lib_path := _lib_path) and Path(lib_path).exists():
+                return Path(lib_path)
+        print("[red]Unable to determine default calibre library path.[/red]")
+        raise typer.Exit(5)
+
+
+    @cli.command()
+    def backup(
+        calibre_library: Path = typer.Argument(
+            default_factory=_get_default_calibre_library_path,
+            help="calibre library directory to backub",
+            exists=True,
+            file_okay=False,
+            dir_okay=True,
+        ),
+    ) -> None:
+        """
+        Export the default calibre library to a tmpdir, create
+        bz2 compressed tarball and optionally upload it to mega
+        """
+        with tempfile.TemporaryDirectory() as tmpdir_name:
+            export_library_dir = (Path(tmpdir_name) / "CalibreLibray").absolute()
+            export_library_dir.mkdir()
+
+            if calibre_library.exists():
+                print(f"[green]✓[/green] Exporting library at {calibre_library} into {export_library_dir}")  # noqa: E501
+
+            subprocess.run(
+                [
+                    "${pkgs.calibre}/bin/calibredb",  # noqa: E501
+                    "export",
+                    "--all",
+                    f"--to-dir={export_library_dir.as_posix()}",
+                ],
+                capture_output=True,
+                text=True,
+                check=True,
+            )
+            now = datetime.now().strftime("%Y-%m-%d")
+            tmpdir = Path(tmpdir_name)
+            tarball = (tmpdir / f"CalibreLibrary_on_{now}.tar.bz2").absolute()  # noqa: E501
+            print(f"[green]✓[/green] Compressing as {tarball}")
+            subprocess.run(
+                [
+                    "${pkgs.gnutar}/bin/tar",  # noqa: E501
+                    "--create",
+                    "--bzip2",
+                    "--preserve-permissions",
+                    "--file",
+                    f"{tarball.as_posix()}",
+                    f"{export_library_dir.as_posix()}",
+                ],
+                capture_output=True,
+                text=True,
+                check=True,
+            )
+            if Confirm.ask("Upload to mega?"):
+                print(f"[green]✓[/green] Uploading {tarball.absolute()} to mega at /calibre_library_backups/{tarball.name}")  # noqa: E501
+                subprocess.run(
+                    [
+                        "${pkgs.megacmd}/bin/mega-put",  # noqa: E501
+                        f"{tarball.absolute().as_posix()}",
+                        f"/calibre_library_backups/{tarball.name}",  # noqa: E501
+                    ],
+                    capture_output=True,
+                    text=True,
+                    check=True,
+                )
+
+            else:
+                print(f"[green]✓[/green] Completed. Moving tarball to {Path.cwd().absolute()}.")  # noqa: E501
+                shutil.move(tarball.as_posix(), ".")
+
+            print(f"[green]✓[/green] Removing all temporary files at {tmpdir.absolute()}")  # noqa: E501
+
+
+    if __name__ == "__main__":
         cli()
   '';
 in {
