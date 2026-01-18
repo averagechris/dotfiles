69eaa8832446e4f3649a5706f57cfc355051ed83
chris <chris@thesogu.com>
2026-01-17 22:01:24 -0600

    fix(darwin-modules): use flakes-native home-manager import
    - Replace legacy <home-manager/nix-darwin> angle-bracket import
    - Remove outdated hardcoded darwinConfig path

--- flakes/darwin-modules/modules/configuration.nix
+++ flakes/darwin-modules/modules/configuration.nix
@@ -1,19 +1,17 @@
 {
   config,
+  inputs,
   pkgs,
   ...
 }: {
   imports = [
-    <home-manager/nix-darwin>
+    inputs.home-manager.darwinModules.home-manager
   ];
 
   # Used for backwards compatibility, please read the changelog before changing.
   # $ darwin-rebuild changelog
   system.stateVersion = 4;
 
-  # Use a custom configuration.nix location.
-  # $ darwin-rebuild switch -I darwin-config=$HOME/.config/nixpkgs/darwin/configuration.nix
-  environment.darwinConfig = "$HOME/projects/dotfiles/hosts/darwin/configuration.nix";
   services.nix-daemon.enable = true;
   nix.package = pkgs.nix;
 
