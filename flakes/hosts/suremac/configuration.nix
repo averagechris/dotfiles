649144c716efbb93d9d7f362136a10781ff1b391
chris <chris@thesogu.com>
2026-03-11 14:21:15 -0700

    feat(suremac): add GitHub CLI (gh) to home-manager packages

--- flakes/hosts/suremac/configuration.nix
+++ flakes/hosts/suremac/configuration.nix
@@ -223,7 +223,7 @@
     programs.waybar.enable = false;
     programs.windsurf.enable = false; # this is overlayed into windsurf
 
-    home.packages = with pkgs; [raycast postman];
+    home.packages = with pkgs; [raycast postman gh];
   };
 
   fonts.packages = [pkgs.nerd-fonts.droid-sans-mono];
