12deb44bce1cc646b3d805e4a548979ebceff24c
chris <chris@thesogu.com>
2026-07-09 19:24:23 -0700

    chore(deps): bump flake inputs

--- flakes/base-lib/packages/helium-bin.nix
+++ flakes/base-lib/packages/helium-bin.nix
@@ -53,11 +53,11 @@
 }:
 stdenvNoCC.mkDerivation rec {
   pname = "helium-bin";
-  version = "0.14.4.1";
+  version = "0.14.2.1";
 
   src = fetchurl {
     url = "https://github.com/imputnet/helium-linux/releases/download/${version}/helium-${version}-x86_64_linux.tar.xz";
-    hash = "sha256-JBrXfwBc9gughP9tIRN6kD8pX4OQickyBV0tF+kDdmI=";
+    hash = "sha256-Tx7XpzyLxd+KyIPuzMelkBnyiaLPau59nmvjH4C/ubA=";
   };
 
   nativeBuildInputs = [
